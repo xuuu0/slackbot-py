95fafe745ce63667542452b9bb1f786463d03e1a
xuuu0 <subiduba6@gmail.com>
2024-07-14 19:30:34 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+import os
+import logging
+from slack_bolt import App
+from dotenv import load_dotenv
+
+load_dotenv()
+logging.basicConfig(level=logging.DEBUG)
+
+app = App()
+
+@app.message("Hello")
+def say_hello(message, say):
+    user = message['user']
+    say(f"Hello, <@{user}>!")
+
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    try:
+        client.views_publish(
+            user_id=event["user"],
+            view={
+                "type": "home",
+                "callback_id": "home_view",
+
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to your _App's Home_* :tada:"
+                        }
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+                        }
+                    }
+                ]
+            }
+        )
+
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
+
+from flask import Flask, request
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
